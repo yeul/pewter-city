8ca1b2640bfe9c21ef5de0d86c0d7a7fe92545ef
singularitypress <jaypandya2k5@gmail.com>
2019-11-25 16:43:49 -0500

    basic setup with graphql

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const { Keystone } = require('@keystonejs/keystone');
-const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
+const { Keystone } = require("@keystonejs/keystone");
+const { MongooseAdapter } = require("@keystonejs/adapter-mongoose");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { Text } = require("@keystonejs/fields");
 
 const keystone = new Keystone({
   name: "Pewter City",
   adapter: new MongooseAdapter()
 });
 
+keystone.createList("Todo", {
+  fields: {
+    name: { type: Text }
+  }
+});
+
 module.exports = {
-  keystone
+  keystone,
+  apps: [new GraphQLApp()]
 };
